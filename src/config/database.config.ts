9e28e2b3e3d516044d64c98bd5bc84e18275c988
Claudio Junior Familia Morel <cfamilia@mardom.com>
2021-04-26 11:11:40 -0400

    Drop App to Move

--- src/config/database.config.ts
+++
@@ -1,5 +0,0 @@
-
-export const databaseConfig = {
-    type: 'mongodb',
-    url: 'mongodb://localhost/the-chosen-car',
-}
--- src/features/car/models/interfaces/cartype.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from 'mongoose';
-
-export interface ICarType extends Document {
-  readonly name: string;
-  readonly description: string;
-  readonly capacity: number;
-}
